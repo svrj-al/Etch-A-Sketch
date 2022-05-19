7ef74a3ffc12822caf189fe7f980f18cb29c9ff3
svrj-al <alexxx.svr@yandex.ru>
2022-05-19 15:13:57 +0300

    Add button to set size of grid, hover on div make them black

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
 const container = document.querySelector('#container');
 
-let gridSize = 16;
-let gridSizeFull = gridSize * gridSize;
+const btn = document.getElementById('button');
+btn.addEventListener('click', () => {
+  getGridSize();
+  draw();
+});
+  
+function getGridSize () {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
-container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); 
+  let gridSize = +prompt("Please enter grid size, less than 65",'');
+  
+  while (gridSize > 64) {
+    gridSize = +prompt("Please enter grid size, less than 65",'');
+  }
+  const gridSizeFull = gridSize * gridSize;
+  
+  container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); 
     grid-template-rows: repeat(${gridSize}, 1fr);`;
 
-for (i = 0; i < gridSizeFull; i++) {
-const content = document.createElement('div');
-content.classList.add('content');
-container.appendChild(content);
+  for (i = 0; i < gridSizeFull; i++) {
+  const content = document.createElement('div');
+  content.classList.add('content');
+  container.appendChild(content);
+  }  
 }
+
+function draw() {
+  let gridElements = document.querySelectorAll('.content');
+
+  gridElements.forEach((element) => {
+    element.addEventListener('mouseover', () => {
+      element.setAttribute("style", addColor());
+    });
+  });
+}
+
+
+function addColor() {
+  return `background: rgb(0, 0, 0)`;
+}
+
+
+/*
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function randomColor() {
+  return `background: rgb(${random(255)}, ${random(255)} , ${random(255)})`;
+}
+*/
